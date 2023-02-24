5e6663dacacfd01b28a4785d209364ce041884a8
shourovahmd <moh.abuhanifa@gmail.com>
2023-02-24 14:35:22 +0600

    table with ClientRow

---
+++ src/components/ClientRow.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { FaTrash } from "react-icons/fa";
+
+export default function ClientRow({ client }) {
+  return (
+    <tr>
+      <td>{client.name}</td>
+      <td>{client.email}</td>
+      <td>{client.phone}</td>
+      <td>
+        <button className="btn btn-danger btn-sm">
+          <FaTrash />
+        </button>
+      </td>
+    </tr>
+  );
+}
--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,5 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
 import React from "react";
+import ClientRow from "./ClientRow";
 
 const GET_CLIENTS = gql`
   query getClients {
@@ -14,8 +15,28 @@ const GET_CLIENTS = gql`
 
 export default function Clients() {
   const { loading, error, data } = useQuery(GET_CLIENTS);
-  console.log(data);
   if (loading) return <p>Loading</p>;
   if (error) return <p>Something went wrong</p>;
-  return <div>{!loading && !error && <h1>Clients</h1>}</div>;
+  return (
+    <>
+      {!loading && !error && (
+        <table className="table table-hover mt-3">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+                data.clients.map(client =>(
+                    <ClientRow key={client.id} client={client}/>
+                ))
+            }
+          </tbody>
+        </table>
+      )}
+    </>
+  );
 }
