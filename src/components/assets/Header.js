24ff739186fef02c977dc3bad913125795993b7f
shourovahmd <moh.abuhanifa@gmail.com>
2023-02-24 12:06:01 +0600

    Header

---
+++ src/components/assets/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.png";
+export default function Header() {
+  return (
+    <nav className="navbar bg-light mb-4 p-0">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          <div className="d-flex">
+            <img src={logo} alt="logo" className="mr-2" />
+            <div>MERN GraphQL</div>
+          </div>
+        </a>
+      </div>
+    </nav>
+  );
+}
