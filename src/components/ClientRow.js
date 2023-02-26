cc835929eaacdcf22f84a2fe452f50a6da89848b
shourovahmd <moh.abuhanifa@gmail.com>
2023-02-26 13:42:05 +0600

    update

--- src/components/ClientRow.js
+++ src/components/ClientRow.js
@@ -7,7 +7,18 @@ import { DELETE_CLIENT } from "./mutation/clientMutations";
 export default function ClientRow({ client }) {
   const [deleteClient] = useMutation(DELETE_CLIENT, {
     variables: { id: client.id },
-    refetchQueries: [{ query: GET_CLIENTS }],
+    // refetchQueries: [{ query: GET_CLIENTS }],
+    update(cache, { data: { deleteClient } }) {
+      const { clients } = cache.readQuery({
+        query: GET_CLIENTS,
+      });
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: {
+          clients: clients.filter((client) => client.id !== deleteClient.id),
+        },
+      });
+    },
   });
 
   return (
