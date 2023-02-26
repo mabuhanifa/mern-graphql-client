2b88a93e79a6c8ee36b15da56f1ab202989e21c6
shourovahmd <moh.abuhanifa@gmail.com>
2023-02-26 13:27:13 +0600

    deleteClient working

--- src/components/ClientRow.js
+++ src/components/ClientRow.js
@@ -1,14 +1,22 @@
+import { useMutation } from "@apollo/client";
 import React from "react";
 import { FaTrash } from "react-icons/fa";
+import { GET_CLIENTS } from "./clientQueries";
+import { DELETE_CLIENT } from "./mutation/clientMutations";
 
 export default function ClientRow({ client }) {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: { id: client.id },
+    refetchQueries: [{ query: GET_CLIENTS }],
+  });
+
   return (
     <tr>
       <td>{client.name}</td>
       <td>{client.email}</td>
       <td>{client.phone}</td>
       <td>
-        <button className="btn btn-danger btn-sm">
+        <button className="btn btn-danger btn-sm" onClick={deleteClient}>
           <FaTrash />
         </button>
       </td>
