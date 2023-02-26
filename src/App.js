62a3990a9aea87a48c8dcd03f872769d4958d0ff
shourovahmd <moh.abuhanifa@gmail.com>
2023-02-26 13:38:28 +0600

    cache

--- src/App.js
+++ src/App.js
@@ -3,9 +3,28 @@ import "./App.css";
 import Clients from "./components/Clients";
 import Header from "./components/Header";
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql",
-  cache: new InMemoryCache(),
+  cache,
 });
 
 function App() {
