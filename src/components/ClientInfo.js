8b42e73e34d460d24ceeda77093ce8caef91b826
shourovahmd <moh.abuhanifa@gmail.com>
2023-03-01 14:39:00 +0600

    ClientInfo

---
+++ src/components/ClientInfo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ClientInfo({client}) {
+  return (
+    <div>ClientInfo</div>
+  )
+}
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,12 +1,29 @@
-import { useQuery } from '@apollo/client';
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { GET_PROJECT } from '../queries/projectQueries';
+import { useQuery } from "@apollo/client";
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import ClientInfo from "../components/ClientInfo";
+import Spinner from "../components/Spinner";
+import { GET_PROJECT } from "../queries/projectQueries";
 
 export default function Project() {
-    const { id} = useParams();
-    const { loading, error, data } = useQuery(GET_PROJECT, { variables: { id } });
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(GET_PROJECT, { variables: { id } });
+  if (loading) return <Spinner />;
+  if (error) return <p>Something Went Wrong</p>;
   return (
-    <div>Project</div>
-  )
+    <>
+      {!loading && !error && (
+        <div className="mx-auto w-75 card p-5">
+          <Link className="btn btn-light btn-sm w-25 d-inline ms-auto" to={"/"}>
+            Back
+          </Link>
+          <h1>{data.project.name}</h1>
+          <p>{data.project.description}</p>
+          <h5 className="mt-3">Project Status</h5>
+          <p className="lead">{data.project.status}</p>
+          <ClientInfo client={data.project.client}/>
+        </div>
+      )}
+    </>
+  );
 }
