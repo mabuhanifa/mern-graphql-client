4f5cfc696b9f59b6ba16c636548604591077d9fc
shourovahmd <moh.abuhanifa@gmail.com>
2023-03-01 14:53:26 +0600

    ClientInfo updated

--- src/components/ClientInfo.js
+++ src/components/ClientInfo.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+import { FaEnvelope, FaIdBadge, FaPhone } from "react-icons/fa";
 
-export default function ClientInfo({client}) {
+export default function ClientInfo({ client }) {
   return (
-    <div>ClientInfo</div>
-  )
+    <>
+      <h5 className="mt-5">Client Information</h5>
+      <ul className="list-group">
+        <li className="list-group-item">
+          <FaIdBadge className="icon" />
+          {client.name}
+        </li>
+        <li className="list-group-item">
+          <FaEnvelope className="icon" />
+          {client.email}
+        </li>
+        <li className="list-group-item">
+          <FaPhone className="icon" />
+          {client.phone}
+        </li>
+      </ul>
+    </>
+  );
 }
