27f902f468e5d5dd563e658dfd7dcb2cb223a243
shourovahmd <moh.abuhanifa@gmail.com>
2023-03-02 14:32:27 +0600

    client deleted with project

--- src/components/ClientRow.js
+++ src/components/ClientRow.js
@@ -3,24 +3,26 @@ import React from "react";
 import { FaTrash } from "react-icons/fa";
 import { DELETE_CLIENT } from "../mutation/clientMutations";
 import { GET_CLIENTS } from "../queries/clientQueries";
+import { GET_PROJECTS } from "../queries/projectQueries";
 
 export default function ClientRow({ client }) {
   const [deleteClient] = useMutation(DELETE_CLIENT, {
     variables: { id: client.id },
+    refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],
     // refetchQueries: [{ query: GET_CLIENTS }],
     /* Updating Cache */
-    update(cache, { data: { deleteClient } }) {
-      const { clients } = cache.readQuery({
-        query: GET_CLIENTS,
-      });
+    // update(cache, { data: { deleteClient } }) {
+    //   const { clients } = cache.readQuery({
+    //     query: GET_CLIENTS,
+    //   });
 
-      cache.writeQuery({
-        query: GET_CLIENTS,
-        data: {
-          clients: clients.filter((client) => client.id !== deleteClient.id),
-        },
-      });
-    },
+    //   cache.writeQuery({
+    //     query: GET_CLIENTS,
+    //     data: {
+    //       clients: clients.filter((client) => client.id !== deleteClient.id),
+    //     },
+    //   });
+    // },
   });
 
   return (
