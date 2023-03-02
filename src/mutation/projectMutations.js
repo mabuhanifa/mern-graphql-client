36afd0807789c34c4d4ff38b9a42dcd48743b10d
shourovahmd <moh.abuhanifa@gmail.com>
2023-03-02 13:46:47 +0600

    UPDATE_PROJECT mutation

--- src/mutation/projectMutations.js
+++ src/mutation/projectMutations.js
@@ -33,3 +33,30 @@ export const DELETE_PROJECT = gql`
     }
   }
 `;
+
+export const UPDATE_PROJECT = gql`
+  mutation UpdateProject(
+    $id: ID!
+    $name: String!
+    $description: String!
+    $status: ProjectStatusUpdate!
+  ) {
+    updateProject(
+      id: $id
+      name: $name
+      description: $description
+      status: $status
+    ) {
+      id
+      name
+      description
+      status
+      client {
+        id
+        name
+        email
+        phone
+      }
+    }
+  }
+`;
